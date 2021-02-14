23f2f2660f4a5d3e08bc1450c6a1fed4f9a974f0
Brett Harder <brettharder11@gmail.com>
2021-02-13 22:46:59 -0500

    runner

---
+++ scripts/test_runner.sh
@@ -0,0 +1,5 @@
+nba_predictions \
+parse_data \
+--player-first-name James \
+--player-last-name Harden \
+--years 2019 2020 
--- src/nba_predictions/runner.py
+++ src/nba_predictions/runner.py
@@ -21,7 +21,7 @@ from nba_predictions.player_scrape import getPlayerData
 logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger('nba_predictions')
 
-def parse_args(args):
+def get_parser():
     """
     Main entry point allowing external calls
 
@@ -39,7 +39,33 @@ def parse_args(args):
     # ----------------
     # Main parser args
     # ----------------
-    parser.add_argument(
+    
+    # Probably remove this
+    # parser.add_argument(
+    #     '--player-first-name',
+    #     dest='player_first_name',
+    #     type=str,
+    #     required=True,
+    #     default='James',
+    #     help=(
+    #         'Player first name'
+    #     )
+    # )
+
+    # -------------------------
+    # mode1 parser - parse_data
+    # -------------------------
+    parse_data_desc = (
+        'Parse player data'
+    )
+    parser_parse_data= subparsers.add_parser(
+        'parse_data', help='Parse player data',
+        formatter_class=def_formatter, description=parse_data_desc)
+
+    # --------------------------------------------------
+    # options for mode1 parser - add arguments for mode1
+    # --------------------------------------------------
+    parser_parse_data.add_argument(
         '--player-first-name',
         dest='player_first_name',
         type=str,
@@ -50,7 +76,7 @@ def parse_args(args):
         )
     )
 
-    parser.add_argument(
+    parser_parse_data.add_argument(
         '--player-last-name',
         dest='player_last_name',
         type=str,
@@ -59,9 +85,9 @@ def parse_args(args):
         help=(
             'Player last name'
         )
-    )
+    )    
 
-    parser.add_argument(
+    parser_parse_data.add_argument(
         '--years',
         dest='years',
         nargs='+',
@@ -69,7 +95,7 @@ def parse_args(args):
         help=('Input years as follows: --years 2018 2019 2020')
     )
 
-    parser.add_argument(
+    parser_parse_data.add_argument(
         '--output_dir',
         dest='output_dir',
         type=str,
@@ -77,10 +103,51 @@ def parse_args(args):
         help=('Directory in which to save player data as a csv')
     )    
 
-    return parser.parse_args(args)
+    # -------------------------------
+    # mode2 parser - feature_engineer
+    # -------------------------------
+    feature_engineer_desc = (
+        'Feature Engineer'
+    )
+    parser_feature_engineer= subparsers.add_parser(
+        'feature_engineer', help='Feature engineer given output of parse_data',
+        formatter_class=def_formatter, description=feature_engineer_desc)
+    
+    # ------------------------------------------
+    # options for feature engineer parser - TODO
+    # ------------------------------------------
 
+    # --------------------------
+    # mode3 parser - train_model
+    # --------------------------
+    train_model_desc = (
+        'Train Model'
+    )
+    parser_train_model= subparsers.add_parser(
+        'train_model', help='Train model given output of feature_engineer',
+        formatter_class=def_formatter, description=train_model_desc)
+    
+    # -------------------------------------
+    # options for train_model parser - TODO
+    # -------------------------------------
 
-def main(args):
+    # ----------------------
+    # mode4 parser - predict
+    # ----------------------
+    predict_desc = (
+        'Predict'
+    )
+    parser_predict= subparsers.add_parser(
+        'predict', help='given model make predictions for a new game',
+        formatter_class=def_formatter, description=predict_desc)
+    
+    # ---------------------------------
+    # options for predict parser - TODO
+    # ---------------------------------
+
+    return parser
+
+def main():
     """Main entry point allowing external calls
 
     Define any functionality here using arguments define above
@@ -90,17 +157,23 @@ def main(args):
       args ([str]): command line parameter list
     """
     # Parses args defined above
-    args = parse_args(args)
+    parser = get_parser()
+    args = parser.parse_args(sys.argv[1:])
+    
+    mode = args.command
+    print(mode)
 
-    df_games = getPlayerData(
-        args.player_first_name,
-        args.player_last_name,
-        args.years
-    )
-    print(df_games)
+    # df_games = getPlayerData(
+    #     args.player_first_name,
+    #     args.player_last_name,
+    #     args.years
+    # )
+    # print(df_games)
+
+    print(os.getcwd())
 
 def run():
-    main(sys.argv[1:])
+    main()
 
 if __name__ == '__main__':
     run()
